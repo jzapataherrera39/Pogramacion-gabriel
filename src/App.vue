6e695c351188c058e5a559b82160ef2a4be9351d
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2021-11-29 16:22:27 -0500

    Summary

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,14 @@
 <template>
   <v-app>
-    
-    
+    <router-view> 
 
-    <v-main>
-      <br>
+    </router-view>
     
-      <h1 class="title"><span style="font-size: 58px;">COMPETENCIAS TIC</span> </h1>
-      <h3 class="subtitle">DIDACTICA Y PEDAGOGIA EMERGENTE</h3>
-      <br>
-      
-     <v-img
-      src=""
-      max-width="600"
-    
- 
-    >
-        </v-img>
-   
-   
-  
-<div class="icon">
-      <router-link to="/entrada">
-      <v-btn
-              y-large
-              color="primary"
-              large="15"
-              elevation="30"
-              fab
-            >
-              <v-icon color="white">start</v-icon>
-            </v-btn>
-            </router-link>
-            </div>
-         
-  
-            
-    </v-main>
   </v-app>
 </template>
-
 <script>
 export default {
-  name: 'inicio', 
+  name: 'App', 
   components: {
     
   },
@@ -55,44 +21,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  background: url('') no-repeat center center fixed !important;
-  background-size: cover;
-  
-}
-.title {
-color:  rgb(0, 0, 0);
-text-align: right;
-position: relative;
-  bottom: -200px;
-   right: 200px;
-}
-.subtitle {
-  color:  rgb(0, 0, 0);
-text-align: right;
-position: relative;
-  bottom: -220px;
-   right: 170px;
-   font-size: 35px;
-  
-}
-body{
-  margin: 0; 
-  padding: 0;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-.icon {
-    align-content: right;
-    align-items: right;
-    padding-left: 85%;
-    position: relative;
-  bottom: -190px;
-   right: 300px;
-   font-size: 50px;
-    
-}
-</style>
